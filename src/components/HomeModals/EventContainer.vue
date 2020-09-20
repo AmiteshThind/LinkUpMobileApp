9bb0b5ad196a1e2759775682570e0f6a5d93f67c
Amitesh Thind <thindamitesh@gmail.com>
2020-09-19 23:06:56 -0400

    updated merge

--- src/components/HomeModals/EventContainer.vue
+++ src/components/HomeModals/EventContainer.vue
@@ -2,7 +2,6 @@
   <div>
     <q-card class="my-card">
       <q-card-section>
-<<<<<<< HEAD
         <q-icon
           class="fixed-right q-ma-md"
           color="red-4"
@@ -17,20 +16,6 @@
         <q-scroll-area style="width: 100%; height: 425px;">
             <event v-for="(event,index) in getLinkUps"  :eventData="event" :key='index' />
         </q-scroll-area>
-=======
-              <q-icon class="fixed-right q-ma-md" color="red-4" size="sm" name="cancel" clickable @click="$root.$emit('closeEventContainer')" />
-          <p style="font-size:35px; margin-top:0;" class="q-my-md text-primary" ><b>Events Found</b></p>
-       <q-scroll-area style="width: 100%; height: 425px;" >
-        <event/>
-         <event/>
-          <event/>
-           <event/>
-           <event/>
-       </q-scroll-area>
-        
-        
-        
->>>>>>> 585d44afcfbc728e163748a29ca4157ed6283425
       </q-card-section>
     </q-card>
   </div>
@@ -56,7 +41,6 @@ export default {
   }
 };
 </script>
-<<<<<<< HEAD
 
 <style scoped>
 .my-card {
@@ -64,5 +48,3 @@ export default {
   height: 50rem;
 }
 </style>
-=======
->>>>>>> 585d44afcfbc728e163748a29ca4157ed6283425
